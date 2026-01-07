3c60cc76c7ca8146530ea724e41a7ec777e50e86
erendgr <49555544+erendgr@users.noreply.github.com>
2026-01-07 12:10:28 +0300

    Signals Added

---
+++ Assets/_Game/_Dev/Scripts/Runtime/Core/Events/Signals.cs
@@ -0,0 +1,20 @@
+﻿namespace _Game._Dev.Scripts.Runtime.Core.Events
+{
+    public struct RestartLevelRequestedSignal{ }
+
+    public struct StartGameRequestedSignal { }
+
+    public struct NextLevelRequestedSignal { }
+
+    public struct LevelReadySignal
+    {
+        public readonly LevelSO LevelData;
+        public LevelReadySignal(LevelSO levelData) => LevelData = levelData;
+    }
+
+    public struct TimerUpdatedSignal
+    {
+        public readonly float RemainingTime;
+        public TimerUpdatedSignal(float remainingTime) => RemainingTime = remainingTime;
+    }
+}
