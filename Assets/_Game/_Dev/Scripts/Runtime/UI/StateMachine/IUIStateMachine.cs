4e8d2a0e557ab322ab5a007726653ec8032df2b8
erendgr <49555544+erendgr@users.noreply.github.com>
2026-01-09 23:02:22 +0300

    UI System Updated

---
+++ Assets/_Game/_Dev/Scripts/Runtime/UI/StateMachine/IUIStateMachine.cs
@@ -0,0 +1,7 @@
+﻿namespace _Game._Dev.Scripts.Runtime.UI.StateMachine
+{
+    public interface IUIStateMachine
+    {
+        void ChangeState<T>(object payload = null) where T : States.IUIState;
+    }
+}
---
+++ Assets/_Game/_Dev/Scripts/Runtime/UI/StateMachine/UIStateMachine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _Game._Dev.Scripts.Runtime.Core.Events;
+using _Game._Dev.Scripts.Runtime.UI.States;
+using Zenject;
+
+namespace _Game._Dev.Scripts.Runtime.UI.StateMachine
+{
+    public class UIStateMachine : IUIStateMachine, IInitializable, IDisposable
+    {
+        private readonly SignalBus _signalBus;
+        private readonly List<IUIState> _states;
+        private IUIState _currentState;
+
+        public UIStateMachine(SignalBus signalBus, List<IUIState> states)
+        {
+            _signalBus = signalBus;
+            _states = states;
+        }
+
+        public void Initialize()
+        {
+            _signalBus.Subscribe<LevelReadySignal>(OnLevelReady);
+            _signalBus.Subscribe<GameOverSignal>(OnGameOver);
+            _signalBus.Subscribe<LevelCompletedSignal>(OnLevelCompleted);
+            ChangeState<UIStartState>();
+        }
+        
+        public void Dispose()
+        {
+            _signalBus.TryUnsubscribe<LevelReadySignal>(OnLevelReady);
+            _signalBus.TryUnsubscribe<GameOverSignal>(OnGameOver);
+            _signalBus.TryUnsubscribe<LevelCompletedSignal>(OnLevelCompleted);
+        }
+        
+
+        public void ChangeState<T>(object payload = null) where T : IUIState
+        {
+            _currentState?.Exit();
+            
+            var newState = _states.FirstOrDefault(s => s is T);
+            if (newState != null)
+            {
+                _currentState = newState;
+                _currentState.Enter(payload);
+            }
+        }
+        
+        private void OnLevelReady() => ChangeState<UIGameplayState>();
+        private void OnGameOver() => ChangeState<UIEndGameState>(false);
+        private void OnLevelCompleted() => ChangeState<UIEndGameState>(true);
+    }
+}
