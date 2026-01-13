d8cb2ab2a14627ca107363c16176002368aeb15e
erendgr <49555544+erendgr@users.noreply.github.com>
2026-01-13 12:40:11 +0300

    Color Mapper Added

---
+++ Assets/_Game/_Dev/Scripts/Runtime/Misc/ColorMapper.cs
@@ -0,0 +1,21 @@
+﻿using _Game._Dev.Scripts.Runtime.Level.Models;
+using UnityEngine;
+
+namespace _Game._Dev.Scripts.Runtime.Misc
+{
+    public static class ColorMapper
+    {
+        public static Color GetColorFromEnum(Colors color)
+        {
+            switch (color)
+            {
+                case Colors.Red: return Color.red;
+                case Colors.Green: return Color.green;
+                case Colors.Blue: return Color.blue;
+                case Colors.Yellow: return Color.yellow;
+                case Colors.Purple: return new Color(0.5f, 0, 0.5f);
+                default: return Color.white;
+            }
+        }
+    }
+}
