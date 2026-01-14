3c58753abeda3efa464645deb116040093b86f62
erendgr <49555544+erendgr@users.noreply.github.com>
2026-01-14 14:39:35 +0300

    Configs Added

---
+++ Assets/_Game/_Dev/Scripts/Runtime/Core/PrefabConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace _Game._Dev.Scripts.Runtime.Core
+{
+    [CreateAssetMenu(fileName = "PrefabConfigurationSO", menuName = "Bus Jam/Prefab Configuration SO", order = 2)]
+    public class PrefabConfigurationSO : ScriptableObject
+    {
+        [Header("Character Prefabs")] 
+        public GameObject PassengerPrefab;
+        public GameObject ObstaclePrefab;
+
+        [Header("Bus Prefabs")] public GameObject BusPrefab;
+    }
+}
